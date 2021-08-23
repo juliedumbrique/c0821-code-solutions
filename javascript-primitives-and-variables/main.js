1f7d5ec57fd098491028a8d795fbb3efaed682b0
Julie Dumbrique <julie.dumbrique@gmail.com>
2021-08-23 10:47:13 -0700

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+var fullName = 'Julie Dumbrique';
+var isCool = true;
+var totalPets = 1;
+
+console.log(fullName);
+console.log(isCool);
+console.log(totalPets);
+
+console.log(typeof 'Julie Dumbrique');
+console.log(typeof true);
+console.log(typeof 1);
+
+var unknown = null;
+console.log(unknown);
+console.log(typeof unknown);
+
+var never;
+console.log(never);
+console.log(typeof never);
