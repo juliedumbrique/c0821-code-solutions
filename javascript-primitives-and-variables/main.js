e8582c24c4a762f52aec56fb8b8266b34c4c5fe7
Julie Dumbrique <julie.dumbrique@gmail.com>
2021-08-23 20:26:17 -0700

    logged each of my variables to the console with labels.

--- javascript-primitives-and-variables/main.js
+++ javascript-primitives-and-variables/main.js
@@ -2,18 +2,18 @@ var fullName = 'Julie Dumbrique';
 var isCool = true;
 var totalPets = 1;
 
-console.log(fullName);
-console.log(isCool);
-console.log(totalPets);
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
 
-console.log(typeof 'Julie Dumbrique');
-console.log(typeof true);
-console.log(typeof 1);
+console.log('typeof fullName;', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
 
 var unknown = null;
-console.log(unknown);
-console.log(typeof unknown);
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
 
 var never;
-console.log(never);
-console.log(typeof never);
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
