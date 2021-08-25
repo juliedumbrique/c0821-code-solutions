9481a94ea680fe1ddcfbca614296a9da4aed10b1
Julie Dumbrique <julie.dumbrique@gmail.com>
2021-08-24 21:03:49 -0700

    Completed javascript-functions-practice exercise.

---
+++ javascript-functions-practice/get-average-of-three-numbers.js
@@ -0,0 +1,8 @@
+/* exported getAverageOfThreeNumbers */
+
+function getAverageOfThreeNumbers(x, y, z) {
+  var average = (x + y + z) / 3;
+  return average;
+}
+
+// var getAverageOfThreeNumbersResult = getAverageOfThreeNumbers(2, 4, 6);
